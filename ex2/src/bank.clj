9feeb129b56acc002cb330fa4a95c8b70b9e0058
Amit Rathore <amitrathore@gmail.com>
2011-12-18 15:22:50 -0800

    deleted account ns

--- ex2/src/bank.clj
+++ ex2/src/bank.clj
@@ -1,6 +1,9 @@
 (ns bank
-  (:use account))
+  (:use clojure.contrib.str-utils))
 
+(declare update-account print-account)
+
+;;;;;; bank related
 
 (defn new-bank 
   ([overdraft-balance accounts]
@@ -23,7 +26,33 @@
   (reduce process-transaction bank transactions))
 
 (defn print-bank [{:keys [overall-overdraft accounts]}]
-  (println "Overdraft:" overall-overdraft)
-  (println "Accounts:\n-----------")
+  (println "OVER-DRAFT:" overall-overdraft)
+  (println "ACCOUNTS:\n----------")
   (doseq [a (vals accounts)] 
     (print-account a)))
+
+
+;;;;;; accounts related
+
+(defn run-txn [{:keys [overall-overdraft] :as bank} 
+               {:keys [transactions balance] :as account} 
+               {:keys [amount] :as transaction} 
+               compute]
+  [overall-overdraft (-> account
+                         (assoc :balance (compute balance amount))
+                         (assoc :transactions (conj transactions transaction)))])
+
+(defmulti update-account (fn [bank accnt txn] (:transaction-type txn)))
+
+(defmethod update-account :withdrawal [bank account transaction]
+  (run-txn bank account transaction -))
+
+(defmethod update-account :deposit [bank account transaction]  
+  (run-txn bank account transaction +))
+
+(defn print-account [{:keys [id type transactions balance] :as account}]
+  (println "Account ID:" id type)
+  (println "Balance:" balance)
+  (println "Transactions:")
+  (println (str-join "\n" transactions))
+  (println "----------------"))
