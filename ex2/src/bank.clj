07012f93b94ac52b1c65c6f14278a6a6d7d650b5
Amit Rathore <amitrathore@gmail.com>
2011-12-18 16:18:51 -0800

    ex2 : handling withdrawl, deposits, overdrafts

--- ex2/src/bank.clj
+++ ex2/src/bank.clj
@@ -3,6 +3,8 @@
 
 (declare update-account print-account)
 
+(def BASE-OVERDRAFT-FEE 10)
+
 ;;;;;; bank related
 
 (defn new-bank 
@@ -34,13 +36,42 @@
 
 ;;;;;; accounts related
 
+(defmulti overdraft-percentage-fee :type)
+
+(defmethod overdraft-percentage-fee :checking [account] 0.10)
+
+(defmethod overdraft-percentage-fee :savings [account] 0.075)
+
+(defmethod overdraft-percentage-fee :money-market [account] 0.05)
+
+(defn newly-overdrawn? [old-balance new-balance]
+  (and (neg? new-balance) (< new-balance old-balance)))
+
+(defn- compute-overdraft-withdraw [old-balance new-balance amount-needed percentage-fee overall-overdraft]
+  (if-not (newly-overdrawn? old-balance new-balance)
+    [overall-overdraft 0]
+    (let [new-overall-overdraft (- overall-overdraft amount-needed)]
+      (if (neg? new-overall-overdraft)
+        [overall-overdraft BASE-OVERDRAFT-FEE]
+        [new-overall-overdraft (* amount-needed percentage-fee)]))))
+
+(defn- compute-overdraft-deposit [transaction-type old-balance amount overall-overdraft]
+  (if-not (and (= :deposit transaction-type) (neg? old-balance))
+    overall-overdraft
+    (+ overall-overdraft amount)))
+
 (defn run-txn [{:keys [overall-overdraft] :as bank} 
-               {:keys [transactions balance] :as account} 
-               {:keys [amount] :as transaction} 
+               {:keys [type transactions balance] :as account} 
+               {:keys [transaction-type amount] :as transaction} 
                compute]
-  [overall-overdraft (-> account
-                         (assoc :balance (compute balance amount))
-                         (assoc :transactions (conj transactions transaction)))])
+  (let [new-account-balance (compute balance amount)
+        [new-overall-overdraft overdraft-fee] (compute-overdraft-withdraw balance new-account-balance amount 
+                                                                          (overdraft-percentage-fee account) 
+                                                                          overall-overdraft)
+        new-overall-overdraft (compute-overdraft-deposit transaction-type balance amount new-overall-overdraft)]
+    [new-overall-overdraft (-> account
+                               (assoc :balance (- new-account-balance overdraft-fee))
+                               (assoc :transactions (conj transactions transaction)))]))
 
 (defmulti update-account (fn [bank accnt txn] (:transaction-type txn)))
 
--- ex2/src/test_data.clj
+++ ex2/src/test_data.clj
@@ -16,7 +16,11 @@
 (def SPEC-TX [{:transaction-id 1 :account-id 1 :transaction-type :withdrawal :amount 200}
               {:transaction-id 2 :account-id 2 :transaction-type :withdrawal :amount 300}
               {:transaction-id 3 :account-id 3 :transaction-type :withdrawal :amount 400}
+
               {:transaction-id 4 :account-id 4 :transaction-type :deposit :amount 150}
               {:transaction-id 5 :account-id 5 :transaction-type :deposit :amount 250}
               {:transaction-id 6 :account-id 6 :transaction-type :deposit :amount 350}
+
+              {:transaction-id 7 :account-id 3 :transaction-type :withdrawal :amount 400}
+              {:transaction-id 8 :account-id 3 :transaction-type :deposit :amount 100}
               ])
